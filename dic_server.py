2ec2c0752cb62f5ed5d0ac4d0e1f7c080d448b96
zzq287778061 <287778061@qq.com>
2018-09-28 13:20:08 +0800

    server frame v1.0

--- dic_server.py
+++ dic_server.py
@@ -10,6 +10,7 @@ last_edited: 18-09-27
         2.根据客户端请求的指令，来做出相应的回复
 '''
 from socket import *
+import pymysql
 import os
 from multiprocessing import *
 import sys
@@ -17,19 +18,71 @@ import signal
 import traceback
 
 
-class dic_sys(object):
+class Dic_sys(object):
 
-    pass
+    def __init__(self,c,db):
+        self.__c = c
+        self.__db = db
+
+    def register(self,data):
+        pass
+
+    def login(self,data):
+        pass
+
+    def search(self,data):
+        pass
+
+    def check_history(self,data):
+        pass
+
+
+def fun(c,db):
+    dic = Dic_sys(c,db)
+    while 1:
+        data = c.recv(1024).decode()
+        print('请求是：', data)
+        if (not data) or data[0] == 'E':
+            c.close()
+            sys.exit(0)
+        elif data[0] == 'R':
+            dic.register(data)
+        elif data[0] == 'L':
+            dic.login(data)
+        elif data[0] == 'S':
+            dic.search(data)
+        elif data[0] == 'H':
+            dic.check_history(data)
+
 
 
 def main():
+    # 创建套接字
     HOST = ''
     PORT = 8888
     ADDR = (HOST, PORT)
     s = socket()
     s.bind(ADDR)
     s.listen(8)
-    dic = dic_sys()
+    # 数据库连接
+    db = pymysql.connect('localhost', 'root', 'zzqzzq123456', 'dic_project')
+    # 忽略子进程退出
+    signal.signal(signal.SIG_IGN, signal.SIGCHLD)
+    while 1:
+        try:
+            connfd, addr = s.accept()
+            print('connected with ', addr)
+        except KeyboardInterrupt:
+            s.close()
+            sys.exit('服务器退出')
+        except Exception as e:
+            traceback.print_exc()
+            continue
+        # 创建子进程来处理客户端请求
+        p = Process(target=fun,args=(coonfd,db))
+        p.daemon = 1
+        p.start()
+
 
 
 
